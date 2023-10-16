c4b8e4437b6795275e34fe5de72ff4234159d925
momoko0719 <yifeit2@uw.edu>
2023-10-16 14:09:55 -0700

    add firebase

--- src/index.js
+++ src/index.js
@@ -11,4 +11,20 @@ root.render(
   </React.StrictMode>
 );
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCveLxr9mU3x4RxyDKFO2nkpuiU1HD9HOk",
+  authDomain: "xscape-66601.firebaseapp.com",
+  projectId: "xscape-66601",
+  storageBucket: "xscape-66601.appspot.com",
+  messagingSenderId: "1005450554068",
+  appId: "1:1005450554068:web:4fe306eefe44ccc2a36941"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
