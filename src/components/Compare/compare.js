f6df9b3be5e7c47cfb9bbb5766bd99cc99dc0c18
Stephanie <sw2020@uw.edu>
2023-11-09 12:12:05 -0800

    Add compare header and buttons

--- src/components/Compare/compare.js
+++ src/components/Compare/compare.js
@@ -1,3 +1,23 @@
 import React from 'react';
 
-export default function Compare(props) {}
+export default function Compare(props) {
+    return (
+        <div className="compare">
+            <div className="content">
+                <div className="compare-heading">Compare</div>
+                <div className="row compare-label">
+                <div className="col">
+                    <div class="dropdown">
+                        <label className="compare-button-text">Select first car</label>
+                    </div>
+                </div>
+                <div className="col">
+                    <div class="dropdown">
+                        <label className="compare-button-text">Select second car</label>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
