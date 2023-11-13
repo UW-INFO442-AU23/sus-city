024b6200bfa0193689cc6895c885cf531e21d786
momoko0719 <yifeit2@uw.edu>
2023-11-13 11:58:10 -0800

    add search

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -1,3 +1,18 @@
 import React from 'react';
 
-export default function Search(props) {}
+export default function Search(props) {
+    return (
+        <div className="search">
+            <header className="main-header">
+                <h2>Search</h2>
+            </header>
+            <div className="search-bar">
+                <div className="input-group">
+                    <input type="text" className="form-control" placeholder="Search Your Perfect Car" aria-label="search cars" />
+                    <button className="btn btn-primary" type="button">Search Cars</button>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
