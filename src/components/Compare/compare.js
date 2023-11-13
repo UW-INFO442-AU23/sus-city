595339e297bef7e7b9079b94433326f1e7e980fe
Stephanie <sw2020@uw.edu>
2023-11-13 11:25:40 -0800

    Edit compare page, added Rubik font

--- src/components/Compare/compare.js
+++ src/components/Compare/compare.js
@@ -6,16 +6,16 @@ export default function Compare(props) {
             <div className="content">
                 <div className="compare-heading">Compare</div>
                 <div className="row compare-label">
-                <div className="col">
-                    <div class="dropdown">
-                        <label className="compare-button-text">Select first car</label>
+                    <div className="col">
+                        <div class="dropdown">
+                            <label className="compare-button-text">Select first car</label>
+                        </div>
                     </div>
-                </div>
-                <div className="col">
-                    <div class="dropdown">
-                        <label className="compare-button-text">Select second car</label>
+                    <div className="col">
+                        <div class="dropdown">
+                            <label className="compare-button-text">Select second car</label>
+                        </div>
                     </div>
-                </div>
                 </div>
             </div>
         </div>
