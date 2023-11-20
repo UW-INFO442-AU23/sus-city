fff54362915adbca8267f5ec60d96266d609e73c
CiciZhao02 <xingyz5@uw.edu>
2023-11-20 01:15:23 -0800

    add quiz filter

--- src/components/Quiz/questionary.js
+++ src/components/Quiz/questionary.js
@@ -13,7 +13,7 @@ const quiztitle = [
 const quizzes = [
   ['Under $30,000', '$30,000 to $35,000', '$35,000 to $40,000', '$40,000 to $45,000', 'above $45,000','No preference'],
   ['Electric Vehicle (EV)', 'Hybrid Vehicle', 'Gas Vehicle (Gas)', 'EV and Hybrid', 'Gas and Hybrid', 'All types of cars'],
-  ['0 g/km CO2 ', 'below 100 g/km CO2', '100 to 200 g/km CO2', 'above 200 g/km CO2','No preference'],
+  ['0 g/km CO2', 'below 100 g/km CO2', '100 to 200 g/km CO2', 'above 200 g/km CO2','No preference'],
   ['100 MPG and above', '75 to 99 MPG', '50 to 74 MPG', '25 to 49 MPG', 'below 25 MPG','No preference'],
   ['More than $6,000', '$4,000 to $6,000', '$2,000 to $4,000', 'Save up to $2,000', 'No preference'],
 ];
--- src/components/Quiz/quizresult.js
+++ src/components/Quiz/quizresult.js
@@ -64,17 +64,109 @@ export const Box = (text) => {
 };
 
 const Results = ({ selectedOptions, cars }) => {
-  const car1 = cars[0]
-  const car2 = cars[1]
+  console.log(cars)
+  console.log(selectedOptions)
+  let priceRange, carTypes, co2EmissionRange, mpgRange, savingsRange;
+
+  // Price Range
+  const priceMapping = {
+    'Under $30,000': [0, 30000],
+    '$30,000 to $35,000': [30000, 35000],
+    '$35,000 to $40,000': [35000, 40000],
+    '$40,000 to $45,000': [40000, 45000],
+    'above $45,000': [45000, Infinity],
+    'No preference': [0, Infinity]
+  };
+  priceRange = priceMapping[selectedOptions[0]];
+
+  // Car Types
+  const carTypeMapping = {
+    'Electric Vehicle (EV)': ['EV'],
+    'Hybrid Vehicle': ['Hybrid'],
+    'Gas Vehicle (Gas)': ['Gas'],
+    'EV and Hybrid': ['EV', 'Hybrid'],
+    'Gas and Hybrid': ['Gas', 'Hybrid'],
+    'All types of cars': ['EV', 'Hybrid', 'Gas']
+  };
+  carTypes = carTypeMapping[selectedOptions[1]];
+
+  // CO2 Emission Range
+  const co2Mapping = {
+    '0 g/km CO2': [0, 0],
+    'below 100 g/km CO2': [0, 100],
+    '100 to 200 g/km CO2': [100, 200],
+    'above 200 g/km CO2': [200, Infinity],
+    'No preference': [0, Infinity]
+  };
+  co2EmissionRange = co2Mapping[selectedOptions[2]];
+
+  // MPG Range
+  const mpgMapping = {
+    '100 MPG and above': [100, Infinity],
+    '75 to 99 MPG': [75, 99],
+    '50 to 74 MPG': [50, 74],
+    '25 to 49 MPG': [25, 49],
+    'below 25 MPG': [0, 25],
+    'No preference': [0, Infinity]
+  };
+  mpgRange = mpgMapping[selectedOptions[3]];
+
+  // Savings Range
+  const savingsMapping = {
+    'More than $6,000': [6000, Infinity],
+    '$4,000 to $6,000': [4000, 6000],
+    '$2,000 to $4,000': [2000, 4000],
+    'Save up to $2,000': [0, 2000],
+    'No preference': [0, Infinity]
+  };
+  savingsRange = savingsMapping[selectedOptions[4]];
+
+  console.log(priceRange)
+  console.log(carTypes)
+  console.log(co2EmissionRange)
+  console.log(mpgRange)
+  console.log(savingsRange)
+  
+
+  const isInRange = (value, range) => value >= range[0] && value <= range[1];
+
+  const filteredCars = cars.filter(car => {
+    const price = parseFloat(car.price.replace(/[^0-9.]/g, ''));
+    const co2Emission = parseInt(car.co2_emission, 10);
+    const mpg = parseInt(car.combined_MPG, 10);
+    const savings = parseInt(car.you_save_spend_in_5_years, 10);
+
+    return isInRange(price, priceRange) &&
+           carTypes.includes(car.car_type) &&
+           isInRange(co2Emission, co2EmissionRange) &&
+           isInRange(mpg, mpgRange) &&
+           isInRange(savings, savingsRange);
+  });
+
+  filteredCars.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+
+  console.log(filteredCars)
+
+  const car1 = filteredCars.length < 2 ? cars[0] : filteredCars[0]
+  const car2 = filteredCars.length < 2 ? cars[1] : filteredCars[1]
+  console.log(filteredCars.length === 0)
+  console.log(car1)
+  console.log(car2)
   return (
     <div className="quiz-result">
       <div className="title-container">
-        <h2>Your Top Car Recommendations</h2>
+        {filteredCars.length < 1 ? <h1>Sorry, no results found. But here are our recommendations:</h1> : <h1>Your Top Recommendations:</h1>}
       </div>
-      <div className="body-container">
-        {car_card(car1)}
-        {car_card(car2)}
-      </div>
+      {
+        filteredCars.length === 1 ? 
+          <div className="body-container">
+            {car_card(filteredCars[0])}
+          </div> : 
+          <div className="body-container">
+            {car_card(car1)}
+            {car_card(car2)}
+          </div>
+      }
       <div className="link-container">
         <a href="/Compare">
           {Box('Compare Cars')}
